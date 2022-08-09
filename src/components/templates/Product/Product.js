cf2d896cb580d37c67734d342125d2cf2df696f7
i.kłusek <J6ZkqwVUn>
2022-08-09 21:09:38 +0200

    Fix getProduct Axios logic

--- src/components/templates/Product/Product.js
+++ src/components/templates/Product/Product.js
@@ -2,22 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { Wrapper, TopWrapper, BottomWrapper } from './Product.styles';
 import ProductTopContent from 'components/organisms/ProductTopContent/ProductTopContent';
 import ProductBottomContent from 'components/organisms/ProductBottomContent/ProductBottomContent';
-import axios from 'axios';
+import ProductsApi from 'api/products';
 import LoadingAnimation from 'components/atoms/LoadingAnimation/LoadingAnimation';
 
-// const productCode = 734914;
-
 const Product = ({ code }) => {
-    const [product, setProducts] = useState(null);
-    const baseURL = `http://localhost:5000/product/${code}`;
+    const [product, setProduct] = useState(null);
 
     useEffect(() => {
-        axios
-            .get(baseURL)
-            .then(({ data }) => {
-                setProducts(data);
-            })
-            .catch((err) => console.log(err));
+        const fetchProduct = async () => {
+            try {
+                const response = await ProductsApi.get(`/${code}`);
+                setProduct(response.data);
+            } catch (err) {
+                if (err.response) {
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                } else {
+                    console.log(`Error: ${err.message}`);
+                }
+            }
+        };
+
+        setTimeout(() => {
+            fetchProduct();
+        }, 1000);
     }, []);
 
     return (
