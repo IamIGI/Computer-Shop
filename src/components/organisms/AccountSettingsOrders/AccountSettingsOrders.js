44bfc56eceacc6c0c8a13aae458f784ba919ba76
i.kłusek <J6ZkqwVUn>
2022-08-15 23:00:22 +0200

    user order UI | API userOrderHistory

--- src/components/organisms/AccountSettingsOrders/AccountSettingsOrders.js
+++ src/components/organisms/AccountSettingsOrders/AccountSettingsOrders.js
@@ -1,12 +1,28 @@
 import AccountSettings from 'components/templates/AccountSettings/AccountSettings';
-import { Wrapper } from './AccountSettingsOrders.style';
+import {
+    BodySection,
+    DateDecorator,
+    FooterSection,
+    OrderContent,
+    OrderDescription,
+    OrderProducts,
+    PageButton,
+    ProductDescription,
+    ProductImage,
+    Line,
+    TitleSection,
+    Wrapper,
+    ProductImageSmall,
+} from './AccountSettingsOrders.style';
 
 import useAxiosPrivate from 'hooks/useAxiosPrivate';
 import { useNavigate, useLocation } from 'react-router-dom';
 import useAuth from 'hooks/useAuth';
 import { useEffect, useState } from 'react';
 import LoadingAnimation from 'components/atoms/LoadingAnimation/LoadingAnimation';
-import order from 'api/order';
+import { BsBox } from 'react-icons/bs';
+import SectionDescription from 'components/atoms/SectionDescription/SectionDescription';
+import { SectionTitle } from 'components/molecules/AccountData/AccountData.style';
 
 const AccountSettingsOrders = () => {
     const { auth } = useAuth();
@@ -15,22 +31,21 @@ const AccountSettingsOrders = () => {
     const location = useLocation();
     //You finished There #JWT Authentication
     const [orderHistory, setOrderHistory] = useState([]);
+    const [countOrders, setCountOrders] = useState(0);
+    const [pageNr, setPageNr] = useState(1);
 
     useEffect(() => {
         let data = {};
-        console.log('OrderHistory: Begin Searching');
         const getUserOrderHistory = async () => {
             data = {
                 userId: auth.id,
-                pageNr: 1,
+                pageNr,
             };
-            console.log(`Data send: ${data.userId}, ${data.pageNr}`);
             try {
                 //Start for loadingAnimation
                 const response = await axiosPrivate.post('order/history', data);
-                setOrderHistory(response.data);
-                console.log(response.data);
-                console.log('OrderHistory: Success ');
+                setOrderHistory(response.data.orderData);
+                setCountOrders(response.data.orderCount);
                 //end for loadingAnimation
             } catch (err) {
                 console.log('OrderHistory: Fail');
@@ -40,45 +55,149 @@ const AccountSettingsOrders = () => {
             }
         };
         getUserOrderHistory();
-        console.log('OrderHistory: Finish');
-    }, []);
+    }, [pageNr]);
+
+    //set right status name
+    const getStatus = (number) => {
+        switch (number) {
+            case 1:
+                return 'W realizacji';
+            case 2:
+                return 'Wysłane';
+            case 3:
+                return 'Zakończone';
+            default:
+                console.log('Bad number status given');
+                break;
+        }
+    };
+    //set right month name
+    const getDate = (date) => {
+        let day = date.split('.')[2];
+        let month = date.split('.')[1];
+        let year = date.split('.')[0];
+        let monthName = '';
+        switch (month) {
+            case '01':
+                monthName = 'Styczeń';
+                break;
+            case '02':
+                monthName = 'Luty';
+                break;
+            case '03':
+                monthName = 'Marzec';
+                break;
+            case '04':
+                monthName = 'Kwiecień';
+                break;
+            case '05':
+                monthName = 'Maj';
+                break;
+            case '06':
+                monthName = 'Czerwiec';
+                break;
+            case '07':
+                monthName = 'Lipiec';
+                break;
+            case '08':
+                monthName = 'Sierpień';
+                break;
+            case '09':
+                monthName = 'Wrzesień';
+                break;
+            case '10':
+                monthName = 'Październik';
+                break;
+            case '11':
+                monthName = 'Listopad';
+                break;
+            case '12':
+                monthName = 'Grudzień';
+                break;
+
+            default:
+                console.log('Bad date given');
+                break;
+        }
+        return `${day} ${monthName} ${year}`;
+    };
+
+    //set right number of page buttons
+    const buttonClicked = (number) => {
+        setPageNr(number);
+    };
+
+    const countPageButtons = [];
+    for (let i = 1; i <= Math.ceil(countOrders / 5.01); i++) {
+        countPageButtons.push(i);
+    }
 
     return (
         <AccountSettings>
             <Wrapper>
-                <>
-                    <p> Zamowienia</p>
-                    {orderHistory.length === 0 ? (
-                        <p>Koszyk pusty</p>
-                    ) : (
-                        <>
-                            {orderHistory.length}
-                            {console.log(orderHistory.length)}
-                            <br />
-                            <ul>
+                <TitleSection>
+                    <SectionTitle>
+                        <SectionDescription title={'Zamówienia'} icon={<BsBox />} />
+                    </SectionTitle>
+                </TitleSection>
+                <BodySection>
+                    <>
+                        {orderHistory.length === 0 ? (
+                            <p>Koszyk pusty</p>
+                        ) : (
+                            <>
                                 {orderHistory.map((item, index) => (
                                     <>
-                                        <br />
-                                        {console.log(item)}
-                                        <li>{item.transactionInfo.date}</li>
-                                        <li>{item._id}</li>
-                                        <li>{item.transactionInfo.price}</li>
-                                        <li>
-                                            {item.products.map((product, index) => (
-                                                <>
-                                                    <img src={product.prevImg} alt="images of product" />
-                                                    {console.log(product.name)}
-                                                    {product.name}
-                                                </>
-                                            ))}
-                                        </li>
+                                        <OrderContent>
+                                            <OrderDescription>
+                                                <h4>{getStatus(item.status)}</h4>
+                                                {/* <br /> */}
+                                                <DateDecorator>{getDate(item.transactionInfo.date)}</DateDecorator>
+                                                nr {item._id}
+                                                <br />
+                                                <h4>{item.transactionInfo.price} zł</h4>
+                                                {/* <br /> */}
+                                            </OrderDescription>
+                                            <OrderProducts>
+                                                {item.products.map((product, index) => (
+                                                    <>
+                                                        {item.products.length > 1 ? (
+                                                            <ProductImageSmall>
+                                                                <img src={product.prevImg} alt="images of product" />
+                                                            </ProductImageSmall>
+                                                        ) : (
+                                                            <>
+                                                                <ProductImage>
+                                                                    <img
+                                                                        src={product.prevImg}
+                                                                        alt="images of product"
+                                                                    />
+                                                                </ProductImage>
+                                                                <ProductDescription>
+                                                                    <p>{product.name}</p>
+                                                                </ProductDescription>
+                                                            </>
+                                                        )}
+                                                    </>
+                                                ))}
+                                            </OrderProducts>
+                                        </OrderContent>
                                     </>
                                 ))}
-                            </ul>
-                            <br />
-                        </>
-                    )}
-                </>
+                            </>
+                        )}
+                    </>
+                </BodySection>
+                <Line />
+                <FooterSection>
+                    <>
+                        {countPageButtons.map((item, index) => (
+                            <PageButton key={index} onClick={() => buttonClicked(item)}>
+                                {item}
+                            </PageButton>
+                        ))}
+                    </>
+                </FooterSection>
             </Wrapper>
         </AccountSettings>
     );
--- src/components/organisms/AccountSettingsOrders/AccountSettingsOrders.style.js
+++ src/components/organisms/AccountSettingsOrders/AccountSettingsOrders.style.js
@@ -3,6 +3,136 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
     width: 100%;
     height: 100%;
+    padding-top: 5%;
+    display: grid;
+    grid-template-rows: 70px 1fr 1px 100px;
+`;
+
+export const TitleSection = styled.div`
+    height: 100%;
+    width: 100%;
+    grid-row: 1/2;
+    justify-content: left;
+    text-align: left;
+    padding-left: 5%;
+`;
+
+export const BodySection = styled.div`
+    height: 100%;
+    width: 100%;
+    grid-row: 2/3;
     justify-content: center;
     text-align: center;
+`;
+
+export const Line = styled.div`
+    grid-row: 3/4;
+    width: 80%;
+    margin-left: 10%;
+    border: 1px solid ${({ theme }) => theme.colors.lightGrey};
+`;
+
+export const FooterSection = styled.div`
+    height: 100%;
+    width: 100%;
+    grid-row: 4/4;
+    justify-content: right;
+    text-align: right;
+    align-items: right;
+    padding-top: 15px;
+    padding-right: 10%;
+    display: flex;
+    flex-direction: row;
+`;
+
+export const OrderContent = styled.div`
+    height: 175px;
+    width: 90%;
+    margin: 3% 5%;
+    display: grid;
+    grid-template-columns: 0.35fr 0.65fr;
+    border-radius: 40px;
+    border: 1px solid ${({ theme }) => theme.colors.lightGrey};
+    &:hover {
+        border: 1px solid ${({ theme }) => theme.colors.grey};
+        box-shadow: 7px 7px 12px 1px ${({ theme }) => theme.colors.grey};
+        cursor: pointer;
+    }
+`;
+
+export const OrderDescription = styled.div`
+    height: 100%;
+    width: 100%;
+    grid-column: 1/2;
+    border-top-left-radius: 40px;
+    border-bottom-left-radius: 40px;
+    background-color: ${({ theme }) => theme.colors.lightLightGrey};
+    text-align: left;
+    padding-left: 15%;
+`;
+
+export const DateDecorator = styled.div`
+    color: ${({ theme }) => theme.colors.darkGrey};
+    font-weight: 600;
+    margin-bottom: 5px;
+`;
+
+export const OrderProducts = styled.div`
+    height: 100%;
+    width: 100%;
+    justify-content: left;
+    grid-column: 2/2;
+    padding-top: 3%;
+    padding-left: 5%;
+    display: flex;
+    flex-direction: row;
+`;
+export const ProductImage = styled.div`
+    height: 120px;
+    width: auto;
+    img {
+        max-height: 100%;
+        max-width: auto;
+    }
+`;
+export const ProductImageSmall = styled.div`
+    margin-top: 5%;
+    margin-left: 3%;
+    height: 70px;
+    width: auto;
+    img {
+        max-height: 100%;
+        max-width: auto;
+    }
+`;
+
+export const ProductDescription = styled.div`
+    padding-top: 4%;
+    justify-content: center;
+    text-align: left;
+    padding-left: 5%;
+    p {
+        font-size: ${({ theme }) => theme.fontSize.l_plus};
+        /* color: ${({ theme }) => theme.colors.darkGrey}; */
+    }
+`;
+
+export const PageButton = styled.div`
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+    border: 1px solid ${({ theme }) => theme.colors.grey};
+    border-radius: 50%;
+    height: 50px;
+    width: 50px;
+    background-color: ${({ theme }) => theme.colors.lightLightGrey};
+    margin-left: 10px;
+    padding-top: 15px;
+    font-size: ${({ theme }) => theme.fontSize.l};
+
+    &:hover {
+        border: 1px solid ${({ theme }) => theme.colors.black};
+        box-shadow: 1px 1px 12px 1px ${({ theme }) => theme.colors.grey};
+        cursor: pointer;
+    }
 `;
