b93d1f9aa38c22618c30aa7252d3dbb2fe3fcab9
i.kłusek <J6ZkqwVUn>
2022-09-20 00:14:28 +0200

    api logic for webUpdate

--- src/components/molecules/NoRealWebisteAlert/NoRealWebsiteAlert.js
+++ src/components/molecules/NoRealWebisteAlert/NoRealWebsiteAlert.js
@@ -1,46 +1,67 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Wrapper, Icon, Alert, Subtitle, ListSection, Title, GitLink } from './NoRealWebsiteAlert.style';
 import { FiAlertTriangle } from 'react-icons/fi';
+import { getWebUpdates } from 'api/webUpdates';
 
 const NoRealWebsiteAlert = () => {
     const [alert, setAlert] = useState(false);
+    const [waitForFetch, setWaitForFetch] = useState(false);
+    const [updates, setUpdates] = useState({});
+    const lastUpdate = updates[updates.length - 1];
+
+    useEffect(() => {
+        const fetchWebUpdates = async (code) => {
+            setWaitForFetch(true);
+            const response = await getWebUpdates();
+            setUpdates(response);
+            setWaitForFetch(false);
+        };
+
+        fetchWebUpdates();
+    }, []);
 
     return (
-        <Wrapper onMouseOver={() => setAlert(true)}>
-            <Icon>
-                <FiAlertTriangle />
-            </Icon>
-            {alert ? (
-                <Alert onMouseOut={() => setAlert(false)}>
-                    <Title>This is website is not real e-commerce website !!</Title>
-                    <Subtitle>Date: 13.09.2022</Subtitle>
-                    <Subtitle>Things implemented last patch:</Subtitle>
-                    <ListSection>
-                        <ul>
-                            <li>Trigger function for HotShootPromotion</li>
-                            <li>Alert about "this is not real website"</li>
-                            <li>Info "be log in to save order"</li>
-                            <li>upgrade css</li>
-                            <li>Delete given product from cart</li>
-                            <li>Buy without being logged</li>
-                            <li>Navigate after authentication</li>
-                        </ul>
-                    </ListSection>
-                    <Subtitle>InProgress:</Subtitle>
-                    <ListSection>
-                        <ul>
-                            <li>RWD - hide accountPreview on minWidth</li>
-                            <li>Cleaner code - make external function for component</li>
-                        </ul>
-                    </ListSection>
-                    <GitLink>
-                        <a href="https://github.com/users/IamIGI/projects/1/views/1">Project table</a>
-                    </GitLink>
-                </Alert>
-            ) : (
+        <>
+            {waitForFetch ? (
                 <></>
+            ) : (
+                <Wrapper onMouseOver={() => setAlert(true)}>
+                    <Icon>
+                        <FiAlertTriangle />
+                    </Icon>
+                    {alert ? (
+                        <Alert onMouseOut={() => setAlert(false)}>
+                            <Title>This is website is not real e-commerce website !!</Title>
+
+                            <Subtitle>Date: {lastUpdate.date}</Subtitle>
+                            <Subtitle>Things implemented last patch:</Subtitle>
+                            <br />
+                            <Subtitle>Added:</Subtitle>
+                            <ListSection>
+                                <ul>
+                                    {lastUpdate.changes.added.map((item) => (
+                                        <li>{item}</li>
+                                    ))}
+                                </ul>
+                            </ListSection>
+                            <Subtitle>Fixed:</Subtitle>
+                            <ListSection>
+                                <ul>
+                                    {lastUpdate.changes.fixes.map((item) => (
+                                        <li>{item}</li>
+                                    ))}
+                                </ul>
+                            </ListSection>
+                            <GitLink>
+                                <a href="https://github.com/users/IamIGI/projects/1/views/1">Project table</a>
+                            </GitLink>
+                        </Alert>
+                    ) : (
+                        <></>
+                    )}
+                </Wrapper>
             )}
-        </Wrapper>
+        </>
     );
 };
 
