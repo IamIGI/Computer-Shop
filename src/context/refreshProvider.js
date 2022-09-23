d98dfb6a9f8256b84478918fb68af547a3e0fac2
i.kłusek <J6ZkqwVUn>
2022-09-23 23:28:51 +0200

    amendments affter CR

--- src/context/refreshProvider.js
+++ src/context/refreshProvider.js
@@ -4,7 +4,6 @@ const RefreshContext = createContext({});
 
 export const RefreshProvider = ({ children }) => {
     const [refresh, setRefresh] = useState(false);
-    console.log(`Refresh context: ${refresh}`);
 
     return <RefreshContext.Provider value={{ refresh, setRefresh }}>{children}</RefreshContext.Provider>;
 };
