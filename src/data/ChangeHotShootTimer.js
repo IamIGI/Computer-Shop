5761de19ae471220a8b51c18bfbe038300b8c258
i.kłusek <J6ZkqwVUn>
2022-09-29 21:49:15 +0200

    added useRegresh context to productPreview

--- src/data/ChangeHotShootTimer.js
+++ src/data/ChangeHotShootTimer.js
@@ -1,4 +1,3 @@
-// import { changeHotShootPromotion } from 'api/hotShoot';
 import useRefresh from 'hooks/useRefresh';
 
 const ChangeHotShootTimer = () => {
@@ -10,18 +9,8 @@ const ChangeHotShootTimer = () => {
         let hours = time.split(':')[0];
         let minutes = time.split(':')[1];
         let seconds = time.split(':')[2];
-        if ((hours === '10' || hours === '22') && minutes === '00' && seconds === '03') {
+        if ((hours === '10' || hours === '22') && minutes === '00' && seconds === '00') {
             setRefresh(!refresh);
-            // const changeHotShoot = async () => {
-            //     try {
-            //         await changeHotShootPromotion();
-            //         setRefresh(!refresh);
-            //     } catch (err) {
-            //         console.log(err);
-            //     }
-            // };
-
-            // changeHotShoot();
         }
     }, 1000);
 
