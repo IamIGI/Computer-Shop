cc8969e0f21c662d5345aa879fd0601132cfe135
i.kłusek <J6ZkqwVUn>
2022-11-24 21:59:13 +0100

    added PopUp free delivery component

---
+++ src/components/organisms/PopUpFreeDelivery/PopUpFreeDelivery.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useRef } from 'react';
+import { useState } from 'react';
+import {
+    Image,
+    Info,
+    InfoCode,
+    InfoDescription,
+    InsideWrapper,
+    ProductName,
+    ProductSection,
+    Title,
+    Wrapper,
+} from './PopUpFreeDelivery.style';
+import toast from 'react-hot-toast';
+
+const PopUpFreeDelivery = ({ product }) => {
+    function copyToClipboard(code) {
+        navigator.clipboard.writeText(code);
+        toast.success('Skopiowano kod', {
+            duration: 2000,
+        });
+    }
+
+    return (
+        <Wrapper>
+            <Title>Oblicz ratę</Title>
+            <InsideWrapper>
+                <ProductSection>
+                    <Image>
+                        <img src={product.prevImg} alt="product" />
+                    </Image>
+                    <ProductName>{product.name}</ProductName>
+                </ProductSection>
+                <Info>
+                    <InfoDescription>Tylko dzisiaj darmowa dostawa z kodem:</InfoDescription>
+                    <InfoCode onClick={() => copyToClipboard('DELIVERY_24')}>DELIVERY_24</InfoCode>
+                </Info>
+            </InsideWrapper>
+        </Wrapper>
+    );
+};
+
+export default PopUpFreeDelivery;
