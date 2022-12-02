84f40201c78eb03cb6f3644e7823c3a05169177a
i.kłusek <J6ZkqwVUn>
2022-12-02 23:10:00 +0100

    clear react warnings

--- src/components/atoms/DropDownMenuSection/DropDownMenuSection.js
+++ src/components/atoms/DropDownMenuSection/DropDownMenuSection.js
@@ -16,11 +16,11 @@ const DropDownMenuSection = ({ icon, description, smallScreen, children }) => {
         <Wrapper onMouseEnter={() => setToggle(true)} onMouseLeave={() => setToggle(false)}>
             <DivRelative>
                 {/** $ - prevent the prop being passed to the DOM element */}
-                <ActiveDropDown $display={toggle}>
+                <ActiveDropDown display={toggle ? 'true' : undefined}>
                     <span>{smallScreen ? <BigIcon>{icon}</BigIcon> : <Icon>{icon}</Icon>}</span>
                     {description !== '' && <LinkDescription>{description}</LinkDescription>}
                 </ActiveDropDown>
-                <ProperMenu $display={toggle}>{children}</ProperMenu>
+                <ProperMenu display={toggle ? 'true' : undefined}>{children}</ProperMenu>
             </DivRelative>
         </Wrapper>
     );
